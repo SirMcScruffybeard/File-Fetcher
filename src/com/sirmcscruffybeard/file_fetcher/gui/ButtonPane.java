236e0b251522098a3d6532427228a30df995572c
SirMcScruffybeard <scruffybeardworks@gmail.com>
2018-12-11 21:16:26 -0500

    Added ScrollPane

--- src/com/sirmcscruffybeard/file_fetcher/gui/ButtonPane.java
+++ src/com/sirmcscruffybeard/file_fetcher/gui/ButtonPane.java
@@ -3,17 +3,18 @@ package com.sirmcscruffybeard.file_fetcher.gui;
 import com.sirmcscruffybeard.file_fetcher.logic.PathWorks;
 
 import javafx.geometry.Orientation;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
 
-public class ButtonPane extends FlowPane {
+public class ButtonPane extends ScrollPane{
 
 	private FileButton[] buttons = null;
 	
 	private PathWorks pathWorks = new PathWorks();
 	
+	private FlowPane pane = new FlowPane(Orientation.VERTICAL);
+	
 	public ButtonPane(PathField inField) {
-		
-		this.setOrientation(Orientation.VERTICAL);
 		
 		this.prepButtons(inField);
 		
@@ -30,6 +31,11 @@ public class ButtonPane extends FlowPane {
 		for (int i = 0; i < buttons.length; i++) buttons[i] = new FileButton(names[i], inField);
 	}
 	
-	private void addButtons() {for(int i = 0; i < buttons.length; i++) this.getChildren().add(buttons[i]); }
+	private void addButtons() {
+		
+		for(int i = 0; i < buttons.length; i++) pane.getChildren().add(buttons[i]); 
+		
+		this.setContent(pane);
+	}
 	
 }
